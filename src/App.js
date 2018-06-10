87759799e9c65387ae6b4e97ed368ed4bf422c47
Jay Kid <cyph3rfox@gmail.com>
2018-06-10 20:59:18 +0200

    chore: import SVG with a loader
    This way I am able to modify its properties thru CSS : )

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+/* eslint import/no-webpack-loader-syntax: off */
 import React, { Component } from 'react';
 import TimePicker from './components/TimePicker';
-import logo from './logo.svg';
+import Logo from '!svg-react-loader?name=Logo!./logo.svg'
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img className="App-logo" alt={'Relax App'} src={logo}/>
+          <Logo aria-hidden="true" className="App-logo"/>
         </header>
         <main className="App-main">
           <TimePicker />
